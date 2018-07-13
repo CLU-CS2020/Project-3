0ed91ffe51624663a55bbae14c4b53acbcb5013a
Ryan <ryan.mason814@gmail.com>
2018-07-12 19:51:39 -0700

    create Network.java
    Professor code

---
+++ Network.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright OrangeDog LLC.
+ * All rights reserved.
+ */
+package zzz;
+
+import java.io.ByteArrayOutputStream;
+import java.io.ObjectOutputStream;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.util.Random;
+
+/**
+ *
+ * @author graham
+ */
+public class Network
+{
+	private final Random random;
+	private final double lossRate;
+
+	public Network(Random random, double failureRate)
+	{
+		this.random = random;
+		this.lossRate = failureRate;
+	}
+
+	public void send(DatagramSocket socket, String hostName, int destPort, Segment segment) throws Exception
+	{
+		if (random.nextDouble() > lossRate)
+		{
+			ByteArrayOutputStream b_stream = new ByteArrayOutputStream();
+			ObjectOutputStream o_stream = new ObjectOutputStream(b_stream);
+			o_stream.writeObject(segment);
+			byte[] data = b_stream.toByteArray();
+			DatagramPacket msg = new DatagramPacket(data, data.length, InetAddress.getByName(hostName), destPort);
+			socket.send(msg);
+		}
+	}
+
+	public void send(DatagramSocket socket, String hostName, int destPort, Segment... segments) throws Exception
+	{
+		for (Segment segment : segments)
+		{
+			send(socket, hostName, destPort, segment);
+		}
+	}
+}
