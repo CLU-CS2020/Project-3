805922a2c1bf187516c51d0b12dd94a18dfed7de
Ryan <ryan.mason814@gmail.com>
2018-07-20 14:48:19 -0700

    Updated TCP Reciever
    What should be the final update for this project.

--- TCP_Receiver.java
+++ TCP_Receiver.java
@@ -14,33 +14,13 @@ public class TCP_Receiver {
     private static int destport = 5432;
     private static final int timeout = 15000; // time in milliseconds
 
-
-    private static Segment listen(DatagramSocket s) throws IOException, ClassNotFoundException {
-        Segment incomingSegment;
-
-        // incoming SYN segment
-        int bufsize = 512;
-        DatagramPacket incomingMSG = new DatagramPacket(new byte[bufsize], bufsize);
-        incomingMSG.setLength(bufsize);  // max received packet size
-        s.receive(incomingMSG);          // the actual receive operation
-        System.err.println("message from <" + incomingMSG.getAddress().getHostAddress()
-                + "," + incomingMSG.getPort() + ">");
-        byte[] data = incomingMSG.getData();
-        ByteArrayInputStream in = new ByteArrayInputStream(data);
-        ObjectInputStream is = new ObjectInputStream(in);
-        incomingSegment = (Segment) is.readObject();
-        System.out.println("**DEBUG** Incoming Segment: " + incomingSegment.toString());
-        return incomingSegment;
-
-    }
-
+    static Random random = new Random(0);
+    static Network network = new Network(random, 0.5);
     private static void sendSYN_ACK(DatagramSocket s) throws SocketException { // sends guaranteed message
-        Random random = new Random(0);
-        Network network = new Network(random, 0);
         System.out.println("SYN Received");
         try {
             Segment sendACK = new Segment(true, true, 0, 1, 1);
-            network.sendGuaranteed(s, SenderIP, destport, sendACK);
+            network.send(s, SenderIP, destport, sendACK);
             System.out.println("ACK sent!");
         } catch (Exception e) {
             e.printStackTrace();
@@ -48,12 +28,10 @@ public class TCP_Receiver {
     }
 
     private static void sendACK(int totalSegments, int seqNo, int ackNo, int length, DatagramSocket s) throws Exception {// sends guaranteed message
-        Random random = new Random(0);
-        Network network = new Network(random, 0);
         System.out.println("Received " + totalSegments + " Segment(s) from sender.");
         String senderIP = "10.100.39.163";
         try {
-            network.sendGuaranteed(s, senderIP, destport, new Segment(false, true, seqNo, ackNo, length));
+            network.send(s, senderIP, destport, new Segment(false, true, seqNo, ackNo, length));
             System.out.println("**DEBUG** Outoing ackNo: " + ackNo);
         } catch (Exception e) {
             e.printStackTrace();
@@ -71,46 +49,55 @@ public class TCP_Receiver {
         int length = 0;
 
         while (true) try {
-            Segment incomingSegment = listen(s);
+            Segment incomingSegment = network.receive(s);//)listen(s);
             if (incomingSegment.isSyn()) {
                 ackNo += incomingSegment.getLength();
-                seqNo = incomingSegment.getAckNo();
                 sendSYN_ACK(s);
-            } else {
-                System.out.println("NO SYN Message received.");
+                seqNo = 1;
+                continue;
             }
-            try {
-                s.setSoTimeout(timeout);       // set timeout in milliseconds
-            } catch (SocketException se) {System.err.println("socket exception: timeout not set!");}
 
             // add received ACK code,
             if (incomingSegment.isAck()) {
                 System.out.println("ACK Received. \n" + incomingSegment.toString());
+                continue;
             }
 
             if (!incomingSegment.isSyn() && !incomingSegment.isAck()) {                         /* add else code for this statement */
-                if (!SegmentMap.containsKey(incomingSegment.getSeqNo())) {                      // if incoming segment doesn't have a key that matches Map,
+                if (!SegmentMap.containsKey(incomingSegment.getSeqNo())) {                      // if incoming
+                    // segment doesn't have a key that matches Map,
                     SegmentMap.put(incomingSegment.getSeqNo(), incomingSegment.getLength());    // add it to map
-                    totalSegments++;                                                            // increment # of segments received.
-                }
-                for (int i = 0; i <= totalSegments; i++) {
-                    if (SegmentMap.containsKey(ackNo)) {
-                        ackNo += SegmentMap.get(incomingSegment.getSeqNo());                    //Returns the value to which the specified key is mapped
-                        i++;
-                    }if ((i == totalSegments) && (SegmentMap.size() == 9)) {
-                        sendACK(totalSegments, seqNo, ackNo, length, s);                           /* if we have everything, send ack*/
-                    }if (SegmentMap.size() != 9 && !SegmentMap.containsKey(ackNo)) {
-                        sendACK(totalSegments, seqNo, ackNo, length, s);                           /* if we don't have all the messages, and no key = ackNo,send ack for what we have */
+                } else {
+                    if (incomingSegment.getSeqNo() < ackNo) {
+                        sendACK(totalSegments, seqNo, ackNo, 0, s);
                     }
                 }
-            }else{
-                /* add else code here */
-                System.out.println("Unknown parameters sent. user error?");
-            }
+                System.out.println("Printing segments in map: " + SegmentMap);
+                int tempAckNo = ackNo;
+                for (int i = 0; i < SegmentMap.size(); i++) {
+                    if (SegmentMap.containsKey(ackNo)) {
+                        System.out.println(SegmentMap.get(ackNo));
+                        ackNo += SegmentMap.get(ackNo);                    //Returns the value
+                        // to which the specified key is mapped
 
+                    }
+                }
+                if (ackNo != tempAckNo) {
+                    sendACK(totalSegments, seqNo, ackNo, 0, s);
+
+                    for (int i = 0; i < ackNo; i++) {
+
+                        if (SegmentMap.containsKey(i)) {
+                            SegmentMap.remove(i);
+                        }
+
+                    }System.out.println("Printing segments in map after removal: " + SegmentMap.keySet());
+                }
+            }
         } catch (Exception e) {
             e.printStackTrace();
             e.getStackTrace();
         }
     }
+
 }
